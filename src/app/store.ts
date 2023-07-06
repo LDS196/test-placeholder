161e3ec679b31df8abcabfc500eaebd1416d884e
LDS196 <lihachevd87@gmail.com>
2023-07-06 18:10:23 +0400

    feat: add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,18 @@
 import { configureStore, ThunkAction, Action, combineReducers } from "@reduxjs/toolkit"
 import { appReducer } from "./app.slice"
 import { postsReducer } from "../features/posts/posts.slice"
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist"
+
+const postsPersistConfig = {
+    key: "posts",
+    storage,
+    blacklist:['users','posts']
+}
 
 const rootReducer = combineReducers({
     app: appReducer,
-    posts: postsReducer,
+    posts: persistReducer(postsPersistConfig, postsReducer),
 })
 
 export const store = configureStore({
@@ -14,7 +22,7 @@ export const store = configureStore({
             serializableCheck: false,
         }),
 })
-
+export const persistor = persistStore(store)
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 import App from "./app/App"
-import { store } from "./app/store"
+import { persistor, store } from "./app/store"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
+import { PersistGate } from "redux-persist/integration/react"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
     <BrowserRouter>
         <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
             <App />
+            </PersistGate>
         </Provider>
     </BrowserRouter>
 )
